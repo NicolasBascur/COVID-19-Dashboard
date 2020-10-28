7045ba22f390a9967bfc49118d82d4929ef0de4f
mvargasinf <mvargasinf@gmail.com>
2020-10-28 14:37:25 -0300

    Metrica Comuna incorporada
    Base de Main.py con las metricas de comuna

--- leer_data.py
+++ leer_data.py
@@ -21,22 +21,33 @@ comunas=["Arica","Camarones","General Lagos","Putre","Desconocido Arica y Parina
 # Implementar panda para cargar los archivos csv
 #
 def datosRegionCumulativo(archivo,region): #directorio archivo , Nombre de region a buscar.
-    tcasos= [[],[]]
-    with open(archivo,'r',encoding='utf-8') as cvs:
-        diccionario = csv.DictReader(cvs)
+    tcasos= [[],[]] # es una lista con 2 arreglos. [Nombre Region][Cantidad de casos] en retroespectiva, podria haberlos hecho pareja
+    with open(archivo,'r',encoding='utf-8') as cvs:# Abrimos el archivo 
+        diccionario = csv.DictReader(cvs) #cargamos el archivo csv como un diccionario
         # headers=diccionario.fieldnames
         #print(region)
-        for datos in diccionario:
+        for datos in diccionario: # Actualmente solo necesitaba el nombre de la region y el numero de casos
             tcasos[0].append(datos['Region'])
             tcasos[1].append(int(float(datos[region])))
     return tcasos
 
-def datosRegionTotales(archivo,region):
+def datosRegionTotales(archivo,region='all'):
     with open(archivo,'r',encoding='utf-8') as cvs:
         diccionario = csv.DictReader(cvs)
         for datos in diccionario:
             if(datos['Region']==region):
                 return(datos)
+            
+
+def casosRegionesTotales(archivo):
+    totalRegiones=[[],[]]
+    with open(archivo,'r',encoding='utf-8') as cvs:
+        diccionario = csv.DictReader(cvs)
+        for datos in diccionario:
+            totalRegiones[0].append(datos['Region'])
+            totalRegiones[1].append(int(datos['Casos totales acumulados']))
+            #totalRegiones.append(datos)#devuelve el diccionario con todas las regiones
+    return totalRegiones 
             
     
 
